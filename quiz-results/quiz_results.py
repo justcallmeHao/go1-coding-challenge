70e0bb971b90870b755ce10b0f6549b473883009
justcallmeHao <haovo.workspace@gmail.com>
2025-09-26 02:34:30 +0800

    quiz result done

--- quiz-results/quiz_results.py
+++ quiz-results/quiz_results.py
@@ -2,6 +2,23 @@ import json
 
 class Solution:
     def easiest_question(self, answers: json, correct: json) -> json:
-        answers = json.loads(answers)
-        correct = json.loads(correct)
+        answers = json.load(answers)
+        correct = json.load(correct)
 
+        count = {i: 0 for i in range(len(correct))}
+
+        index = range(len(correct))
+
+        for ans in answers:
+            check = zip(index, ans, correct)
+            for pair in check:
+                if pair[1] == pair[2]:
+                    count[pair[0]] += 1
+
+        return json.dumps(f"The easiest question is {max(count, key=lambda x: count[x])}")
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.easiest_question(
+        open('./test-3.json', 'r', encoding='utf-8'),
+        open('./test-3-ans.json', 'r', encoding='utf-8')))
